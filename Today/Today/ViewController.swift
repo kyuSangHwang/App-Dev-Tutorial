b2eb8e0e6c7963ea280b5df01041c463082e107e
KS <kyusang.dev@gmail.com>
2024-07-21 14:09:06 +0900

    Initialize Today Project

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 황규상 on 7/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
