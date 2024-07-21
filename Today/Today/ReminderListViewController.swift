2c98a4d90ec1b612872653dc114b15d65a4ac93c
KS <kyusang.dev@gmail.com>
2024-07-21 15:29:48 +0900

    diffable data source snapshot에 대한 타입 별칭 추가

--- Today/Today/ReminderListViewController.swift
+++ Today/Today/ReminderListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ReminderListViewController: UICollectionViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
     
     var dataSource: DataSource!
     
