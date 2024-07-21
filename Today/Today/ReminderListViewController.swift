e5812ec94ce2066fe11389cbb4053c727a9a685a
KS <kyusang.dev@gmail.com>
2024-07-21 14:50:11 +0900

    상위 클래스 변경

--- Today/Today/ReminderListViewController.swift
+++ Today/Today/ReminderListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ReminderListViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
